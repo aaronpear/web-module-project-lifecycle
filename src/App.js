3fec2425abdc30504ce0b684313d98104b7a95a1
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-11-30 14:40:41 -0800

    User component complete and displaying proper data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import User from './components/User';
 import FollowerList from './components/FollowerList';
+import axios from 'axios';
 import styles from './styles.css';
 
 class App extends React.Component {
+  state = {
+    input: '',
+    userData: [],
+    followers: []
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/aaronpear')
+    .then(response => {
+      this.setState({
+        ...this.state,
+        userData: response.data
+      })
+    })
+    .catch(error => {
+      console.error(error)
+    });
+
+    axios.get('https://api.github.com/users/aaronpear/followers')
+    .then(response => {
+      this.setState({
+        ...this.state,
+        followers: response.data
+      })
+    })
+    .catch(error => {
+      console.error(error)
+    });
+  }
+
   render() {
     return(<div className='body-container'>
       <h1>Github Card</h1>
@@ -11,8 +42,8 @@ class App extends React.Component {
         <input type='text' />
         <button>Search</button>
       </form>
-      <User />
-      <FollowerList />
+      <User userData={this.state.userData} />
+      <FollowerList followers={this.state.followers} />
     </div>);
   }
 }
--- src/components/User.js
+++ src/components/User.js
@@ -2,17 +2,22 @@ import React from 'react';
 import styles from '../styles.css';
 
 class User extends React.Component {
+    constructor() {
+        super();
+    }
+
     render() {
+        const { userData } = this.props;
         return (
             <div className='user-container'>
                 <div className='user-pic'>
-                    <img src='' alt='user pic here' />
+                    <img src={userData.avatar_url} alt='user pic here' />
                 </div>
                 <div className='user-data'>
-                    <h2>Username</h2>
-                    <h3>Account Name</h3>
-                    <p>Total Repos: ___</p>
-                    <p>Total Followers: ___</p>
+                    <h2>{userData.name}</h2>
+                    <h3>{userData.login}</h3>
+                    <p>Total Repos: {userData.public_repos}</p>
+                    <p>Total Followers: {userData.followers}</p>
                 </div>
             </div>
         )
