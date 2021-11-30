adbfa3cdcf2627ad0ac5489663aa6522acd454c2
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-11-30 15:50:17 -0800

    MVP Complete

--- src/App.js
+++ src/App.js
@@ -4,15 +4,44 @@ import FollowerList from './components/FollowerList';
 import axios from 'axios';
 import styles from './styles.css';
 
+
 class App extends React.Component {
   state = {
+    currentUser: 'aaronpear',
     input: '',
     userData: [],
     followers: []
   }
 
+  getUserData = (user) => {
+    axios.get(`https://api.github.com/users/${user}`)
+    .then(response => {
+      this.setState({
+        ...this.state,
+        userData: response.data
+      })
+    })
+    .catch(error => {
+      console.error(error)
+    });
+  }
+  
+  getUserFollowers = (user) => {
+    axios.get(`https://api.github.com/users/${user}/followers`)
+    .then(response => {
+      this.setState({
+        ...this.state,
+        followers: response.data
+      })
+    })
+    .catch(error => {
+      console.error(error)
+    });
+  }
+  
+
   componentDidMount() {
-    axios.get('https://api.github.com/users/aaronpear')
+    axios.get(`https://api.github.com/users/aaronpear`)
     .then(response => {
       this.setState({
         ...this.state,
@@ -35,12 +64,35 @@ class App extends React.Component {
     });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.currentUser !== prevState.currentUser) {
+      this.getUserData(this.state.currentUser);
+      this.getUserFollowers(this.state.currentUser);
+    }
+}
+
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.setState({
+      ...this.state,
+      currentUser: this.state.input
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      ...this.state,
+      input: event.target.value
+    })
+  }
+
   render() {
     return(<div className='body-container'>
       <h1>Github Card</h1>
       <form>
-        <input type='text' />
-        <button>Search</button>
+        <input type='text' onChange={this.handleChange} />
+        <button onClick={this.handleClick}>Search</button>
       </form>
       <User userData={this.state.userData} />
       <FollowerList followers={this.state.followers} />
